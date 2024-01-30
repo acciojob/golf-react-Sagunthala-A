dc7cb9c86875aa7e9d25d5230ec4e41408a892d4
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-01-30 11:07:39 +0000

    not completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,59 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+// import React, { Component, useState } from "react";
+// import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
+// class App extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             renderBall: false,
+//             posi : 0,
+//             ballPosition: { left: "0px" }
+//         };
+//         this.renderChoice = this.renderBallOrButton.bind(this)
+//         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+//     };
 
-    buttonClickHandler() {
+//     buttonClickHandler() {
    
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+//    }
+//     renderBallOrButton() {
+// 		if (this.state.renderBall) {
+// 		    return <div className="ball" style={this.state.ballPosition}></div>
+// 		} else {
+// 		    return <button onClick={this.buttonClickHandler} >Start</button>
+// 		}
+//     }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
+//     // bind ArrowRight keydown event
+//     componentDidMount() {
       
-    }
+//     }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div className="playground">
+//                 {this.renderBallOrButton()}
+//             </div>
+//         )
+//     }
+// }
+
+import React, {useState} from 'react';
+import '../styles/App.css'
+
+const App = ()=>{
+    const [start,setStart] = useState(true);
+
+
+    return (
+        <div>
+            { 
+                start? 
+                <button>Start</button> :
+                <div style={{"width":"50px", "height":"50px", "backgroundColor":"black"}}></div>
+            }
+        </div>
+    )
 }
 
 
